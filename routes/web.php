f585e6b83a6348071b3661bd5ce9cdfbffc51454
IsisMartinez2018 <44509236+IsisMartinez2018@users.noreply.github.com>
2018-10-28 18:43:41 -0400

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -48,14 +48,38 @@ Route::get('/contactus', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+Route::get('/users/logout', 'Auth\LoginController@userLogout')->name('user.logout');
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::group(['middleware'=>['auth']], function(){
-Route::post("store", 'ordersController@store');
+Route::get("create", 'orderControllerUser@create');
+Route::post("store", 'orderControllerUser@store');
 
+
+});
+
+
+Route::prefix('/admin')->group(function(){
+	Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+	Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+	Route::get('/', 'AdminController@index')->name('admin.dashboard');
+    Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
+
+    Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+    Route::get('/password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset');
+Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+
+
+
+});
+
+Route::group(['middleware'=>['auth:admin']], function(){
 
 Route::resource('orders', 'ordersController');
 });
